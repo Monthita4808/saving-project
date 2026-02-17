7c002c2b1a2fc78b049d24e6cbba3bfc2d0b4b34
mon1131 <67monthita.pl@gmail.com>
2026-02-17 14:31:13 +0700

    change api to render

--- frontend/script.js
+++ frontend/script.js
@@ -1,7 +1,5 @@
 // frontend/script.js
-
-const API_BASE_URL = 'http://localhost:3000/api';
-
+const API_BASE_URL = 'https://saving-project.onrender.com/api';
 
 // **อัปเดตรายการชื่อผู้ออมใหม่**
 const userNames = ["ออมสิน", "เบนซ์", "แพร", "มิ้ว"]; 
